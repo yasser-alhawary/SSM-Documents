4e9add85761be8a4b53c877fd9e9ef661121f453
yasser <eng.yasser.alhawary@gmail.com>
2022-08-09 11:06:59 +0200

    a

--- get-documents.sh
+++ get-documents.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 #This Document Download AWS Predefined Documents Command/Automation/Session/Policy with YAML Format with naming convention DocumentName_Version
 #The Downloaded Document YAML Format Maybe Broken Because Of paterns within the documents itself
+#v2 reduce api calls
 for DocumentType in Command Automation Policy Session
 	do
 		echo "All $DocumentType start Downloading"
-		for DocumentName in  `aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].Name'  --filter Key=DocumentType,Values=$DocumentType	Key=Owner,Values=Amazon --output text`
+		for DocumentVersion in  $(aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].{name:Name,version:DocumentVersion}'  --filter Key=DocumentType,Values=$DocumentType Key=Owner,Values=Amazon --output text|sed 's/\t/_/g'|sed 's/$/.yaml/g'|egrep -v `ls $DocumentType -1 |tr '\n' '|' |sed 's/.$//'`)
 				do
-					LastVersion=$(aws ssm describe-document --name  $DocumentName  --query Document.DocumentVersion --output text)
-					DocumentFile=$DocumentType/${DocumentName}_${LastVersion}.yaml
-					if [ -e $DocumentFile ]
-						then
-							echo " Last Version of $DocumentName is already exist"
-						else
-							aws ssm get-document   --no-cli-pager  --query Content --document-format YAML --output yaml --name $DocumentName  | sed 's/\\n/\n/g'|sed 's/\\//g'|tail -n +2|head -n -1 > $DocumentFile && echo " $DocumentName Downloaded with version $LastVersion "
-					fi
-					
+					DocumentName=`echo $DocumentVersion |cut -d'_' -f1`
+					aws ssm get-document   --no-cli-pager  --query Content --document-format YAML --output yaml --name $DocumentName  | sed 's/\\n/\n/g'|sed 's/\\//g'|tail -n +2|head -n -1 > $DocumentType/$DocumentVersion && echo " $DocumentName Downloaded with Last version "
 				done
 		echo "All $DocumentType Downloaded"
 	done
