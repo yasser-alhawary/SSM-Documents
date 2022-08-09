64a590d1fa8d674d2f49cf6cdcd8c713fa9a57ad
yasser <eng.yasser.alhawary@gmail.com>
2022-08-09 12:06:39 +0200

    s

--- Readme.md
+++ Readme.md
@@ -1,9 +1,6 @@
-This Repo Contains AWS SSM Predefined Documents in YAML for education purpose:<br />
-    - Command<br />
-    - Automation <br />
-    - Session<br />
-    - Policy<br />
-  Downloaded via get-documents.sh bash script<br />
-  Some Documents YAML Format Maybe broken due to regex and patterns<br /><br />
-  Feel Free to Edit the script to perform better<br />
-  Documents Downloaded with DocumentName_Version.yaml Naming Convention , same docuement name and version shouldnot be downloaded twice<br />
+This Repo Contains AWS SSM Predefined Documents in YAML format inside "Data" Directory for education purpose:<br />
+ Downloaded via get-documents.sh bash script - rerun the script to discover any updates <br />
+ Script Discover Amazon SSM Documents/Templates and download it <br /> 
+ Some Documents YAML Format Maybe broken due to regex and patterns<br /><br />
+ Feel Free to Edit the script to perform better<br />
+ Documents Downloaded with DocumentName_Version.yaml Naming Convention , same docuement name and version shouldnot be downloaded twice<br />
--- get-documents.sh
+++ get-documents.sh
@@ -2,13 +2,15 @@
 #This Document Download AWS Predefined Documents Command/Automation/Session/Policy with YAML Format with naming convention DocumentName_Version
 #The Downloaded Document YAML Format Maybe Broken Because Of paterns within the documents itself
 #v2 reduce api calls
-for DocumentType in Command Automation Policy Session
+cd Data
+for DocumentType in $(aws ssm list-documents --no-cli-pager --filter Key=Owner,Values=Amazon --query DocumentIdentifiers[].DocumentType --output text |tr '\t' '\n'|sort|uniq)
 	do
-		echo "All $DocumentType start Downloading"
-		for DocumentVersion in  $(aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].{name:Name,version:DocumentVersion}'  --filter Key=DocumentType,Values=$DocumentType Key=Owner,Values=Amazon --output text|sed 's/\t/_/g'|sed 's/$/.yaml/g'|egrep -v `ls $DocumentType -1 |tr '\n' '|' |sed 's/.$//'`)
+		echo -e  "######\nAll $DocumentType start Downloading"
+		[ -d $DocumentType ] || mkdir $DocumentType 
+		for DocumentVersion in  $(aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].{name:Name,version:DocumentVersion}'  --filter Key=DocumentType,Values=$DocumentType Key=Owner,Values=Amazon --output text|sed 's/\t/_/g'|sed 's/$/.yaml/g'|tee ${DocumentType}/${DocumentType}_DocumentsVersionsList.txt|egrep -v `ls $DocumentType -1 |tr '\n' '|' |sed 's/.$//'`)
 				do
 					DocumentName=`echo $DocumentVersion |cut -d'_' -f1`
-					aws ssm get-document   --no-cli-pager  --query Content --document-format YAML --output yaml --name $DocumentName  | sed 's/\\n/\n/g'|sed 's/\\//g'|tail -n +2|head -n -1 > $DocumentType/$DocumentVersion && echo " $DocumentName Downloaded with Last version "
+					aws ssm get-document   --no-cli-pager  --query Content --document-format YAML --output yaml --name $DocumentName  | sed 's/\\n/\n/g'|sed 's/\\//g'|tail -n +2|head -n -1 > $DocumentType/$DocumentVersion && echo -e "\t\t$DocumentName Downloaded with Last version "
 				done
-		echo "All $DocumentType Downloaded"
+		echo -e "All $DocumentType Downloaded\n######"
 	done
