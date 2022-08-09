8080b3e841df5f7d448e5a35274550e68fd249dd
yasser <eng.yasser.alhawary@gmail.com>
2022-08-09 16:39:20 +0200

    sa

--- get-documents.sh
+++ get-documents.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#This Document Download AWS Predefined Documents Command/Automation/Session/Policy with YAML Format with naming convention DocumentName_Version
-#The Downloaded Document YAML Format Maybe Broken Because Of paterns within the documents itself
-#v2 reduce api calls
-mkdir -p Data && cd Data
+#This Document Download AWS Predefined Documents Command/Automation/Session/Policy with YAML/JSON Format with naming convention DocumentName_Version
+#v3 add json/yaml as postional parameter option default is yaml
+mkdir -p Data 
+DocumentFormat=`[  "$@" = "json" ] 2> /dev/null && echo -n json || echo -n yaml`
 for DocumentType in $(aws ssm list-documents --no-cli-pager --filter Key=Owner,Values=Amazon --query DocumentIdentifiers[].DocumentType --output text |tr '\t' '\n'|sort|uniq)
 	do
-		echo -e  "######\nAll $DocumentType start Downloading"
-		mkdir -p $DocumentType 
-		for DocumentVersion in  $(aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].{name:Name,version:DocumentVersion}'  --filter Key=DocumentType,Values=$DocumentType Key=Owner,Values=Amazon --output text|sed 's/\t/_/g'|sed 's/$/.yaml/g'|tee ${DocumentType}/${DocumentType}_DocumentsVersionsList.txt|egrep -v `ls $DocumentType -1 |tr '\n' '|' |sed 's/.$//'`)
+		echo -e  "######\nAll \033[1;32m $DocumentType \033[0m start Downloading"
+		mkdir -p Data/${DocumentType}/${DocumentFormat}
+		for DocumentVersion in  $(aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].{name:Name,version:DocumentVersion}'  --filter Key=DocumentType,Values=${DocumentType} Key=Owner,Values=Amazon --output text|sed 's/\t/_/g'|sed "s/\$/.${DocumentFormat}/g"|tee Data/${DocumentType}/${DocumentFormat}/${DocumentType}_Documents_Versions_List.txt|egrep -v `ls Data/$DocumentType/${DocumentFormat} -1 |tr '\n' '|' |sed 's/.$//'`)
 				do
-					DocumentName=`echo $DocumentVersion |cut -d'_' -f1`
-					aws ssm get-document   --no-cli-pager  --query Content --document-format YAML --output yaml --name $DocumentName  | sed 's/\\n/\n/g'|sed 's/\\//g'|tail -n +2|head -n -1 > $DocumentType/$DocumentVersion && echo -e "\t\t$DocumentName Downloaded with Last version "
+					DocumentName=`echo $DocumentVersion |rev | cut -d'_' -f 2- | rev`
+					aws ssm get-document   --no-cli-pager  --query Content --document-format `echo ${DocumentFormat} | tr '[:lower:]' '[:upper:]'` --output text --name $DocumentName  > Data/${DocumentType}/${DocumentFormat}/${DocumentVersion} && echo -e "\t\t\033[1;32m $DocumentName \033[0m Downloaded with Last version "
 				done
-		echo -e "All $DocumentType Downloaded\n######"
+		echo -e "All \033[1;32m $DocumentType \033[0m Downloaded\n######"
 	done
