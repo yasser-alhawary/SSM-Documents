013213aabe480af40ac2531a829ab0a4c431c0bf
yasser <eng.yasser.alhawary@gmail.com>
2022-08-09 17:29:11 +0200

    sa

--- get-documents.sh
+++ get-documents.sh
@@ -10,7 +10,7 @@ for DocumentType in $(aws ssm list-documents --no-cli-pager --filter Key=Owner,V
 		for DocumentVersion in  $(aws ssm list-documents --no-cli-pager --query 'DocumentIdentifiers[].{name:Name,version:DocumentVersion}'  --filter Key=DocumentType,Values=${DocumentType} Key=Owner,Values=Amazon --output text|sed 's/\t/_/g'|sed "s/\$/.${DocumentFormat}/g"|tee Data/${DocumentType}/${DocumentFormat}/${DocumentType}_Documents_Versions_List.txt|egrep -v `ls Data/$DocumentType/${DocumentFormat} -1 |tr '\n' '|' |sed 's/.$//'`)
 				do
 					DocumentName=`echo $DocumentVersion |rev | cut -d'_' -f 2- | rev`
-					aws ssm get-document   --no-cli-pager  --query Content --document-format `echo ${DocumentFormat} | tr '[:lower:]' '[:upper:]'` --output text --name $DocumentName  > Data/${DocumentType}/${DocumentFormat}/${DocumentVersion} && echo -e "\t\t\033[1;32m $DocumentName \033[0m Downloaded with Last version "
+					aws ssm get-document   --no-cli-pager   --document-format `echo ${DocumentFormat} | tr '[:lower:]' '[:upper:]'` --output text --name $DocumentName  > Data/${DocumentType}/${DocumentFormat}/${DocumentVersion} && echo -e "\t\t\033[1;32m $DocumentName \033[0m Downloaded with Last version "
 				done
 		echo -e "All \033[1;32m $DocumentType \033[0m Downloaded\n######"
 	done
